85a972d68e96d2e7e81a70cc6ffa639b1c154378
Mohit Dodhia <mhtdodhia@gmail.com>
2022-03-23 23:30:54 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const port = 3000;
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+
+
+app.use(express.json());
+
+const users = [];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword };
+        users.push(user);
+        
+        res.status(201).send();
+
+    } catch (error) {
+        res.status(500).send();
+    }
+
+});
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name === req.body.name);
+
+    if (user == null) {
+        return res.status(400).send('User not found');
+    }
+
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Logged In');
+        } else {
+            res.send('Please check your credentials');
+        }
+    } catch (error) {
+        res.status(500).send();
+    }
+
+});
+
+
+
+app.listen(port);
